f03c0a18564787ed6cc56fe7e3fdf5b3ac3c2e2d
Guilherme G. Prunes <guilhermegarciaprunes456@gmail.com>
2026-07-20 20:35:55 -0300

    wip: molding & structuring login/authentication

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ restaurante3.delivery('20-30', 0)
 restaurante4 = Restaurante('Los Burguer', 'Hamburgueria')
 restaurante4.delivery('25-35', 7.0)
 
+restaurante5 = Restaurante('Skibidi Toilet Simulator', 'Italiana')
+
 restaurante1.avaliar('Thiago', 4.3, 'Muito bom, ótimo!')
 restaurante1.avaliar('Maria', 4.8, 'Excelente comida!')
 restaurante1.avaliar('João', 4.5, 'Ótimo atendimento!')
@@ -23,7 +25,8 @@ restaurante2.avaliar('Carlos', 3.8, 'Bom, mas poderia melhorar.')
 
 categorias = ['Todos','Italiana', 'Brasileira', 'Japonesa', 'Hamburgueria']
 
-restaurantes = [restaurante1, restaurante2, restaurante3, restaurante4]
+restaurantes = [restaurante1, restaurante2, restaurante3, restaurante4, restaurante5]
+filtro_restaurantes = []
 # 
 
 app = Flask(__name__)
@@ -31,7 +34,25 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     categoria_selecionada = request.args.get('categoria')
-    return render_template('home.html', restaurantes=restaurantes, categorias=categorias, categoria_selecionada=categoria_selecionada)
+    filtro_restaurantes.clear()
+    if not categoria_selecionada or categoria_selecionada == "Todos":
+        return render_template('home.html', restaurantes=restaurantes, categorias=categorias, categoria_selecionada=categoria_selecionada)
+    else:
+        for restaurante in restaurantes:
+            if restaurante._categoria == categoria_selecionada:
+                filtro_restaurantes.append(restaurante)
+        return render_template('home.html', restaurantes=filtro_restaurantes, categorias=categorias, categoria_selecionada=categoria_selecionada) 
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/auth", methods=['GET', 'POST'])
+def autenticar():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    return f'usuario = {usuario} | senha = {senha}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
